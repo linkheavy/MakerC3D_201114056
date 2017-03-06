b71e9f2ca4bfec38871dda8e98e4c40188707ae7
robinson perez <linkheavy@hotmail.com>
2017-03-05 18:06:02 -0600

    Hoja de trabajo 2 Robinson Jonathan Perez Fuentes 201114056

---
+++ src/org/compi2/codigo3d/parser/Lexico.bat
@@ -0,0 +1,6 @@
+SET JAVA_HOME="C:\Program Files\Java\jdk1.8.0_45\bin"
+SET PATH=%JAVA_HOME%;%PATH%
+SET CLASSPATH=%JAVA_HOME%;
+cd C:\Users\robinson\Documents\compiladores\Tarea\MakerC3D\src\org\compi2\codigo3d\parser
+java -jar C:\Users\robinson\Documents\compiladores\Tarea\MakerC3D\lib\JFlex_1.6.1\jflex-1.6.1.jar lexico.flex
+pause
